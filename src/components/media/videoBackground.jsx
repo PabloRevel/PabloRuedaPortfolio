01d62e48bb91dedcef8d4eae1fe94614c26444b4
Pablo <pablors-j@hotmail.com>
2021-01-30 16:44:43 +0100

    update video-img

--- src/components/media/videoBackground.jsx
+++ src/components/media/videoBackground.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import mediaStyles from "./media.module.scss";
 
 const VideoBackground = (props) => {
+    const [isVideoLoaded, setIsVideoLoaded] = React.useState(false);
+    const onLoadedData = () => {
+        setIsVideoLoaded(true);
+      };
+
     return ( 
         
         <header className={mediaStyles.videoHeader}>
-            <video poster="./video/loadingVideo.png" loop muted autoPlay>
+            <img
+                className={mediaStyles.tiny}
+                src={require("./video/loadingVideo.png")}
+                alt="thumb"
+                style={{ opacity: isVideoLoaded ? 0 : 1 }}
+            />
+            <video loop muted autoPlay
+                onLoadedData={onLoadedData}
+                style={{ opacity: isVideoLoaded ? 1 : 0 }}
+            >
                 <source
                 src={require("./video/video.mp4")}
                 type="video/mp4"
