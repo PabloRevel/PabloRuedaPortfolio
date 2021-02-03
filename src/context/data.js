e172ea580be47ec0f9451a8be994280daa95d248
Pablo <pablors-j@hotmail.com>
2021-02-03 10:20:59 +0100

    change meta

--- src/context/data.js
+++ src/context/data.js
@@ -3,20 +3,5 @@
 export const headData = {
   title: 'Pablo Rueda | Front-end Developer',
   lang: 'en', 
-  description: ' Desarrollador front-end en Andalucia, España. Abierto a colaboraciones y proyectos. ¡Si necesitas un sitio web no dudes en contactarme! ', 
-};
-
-// HERO DATA
-export const heroData = {
-  sectionTitle: "MAIN",
-  name: 'Pablo Rueda',
-  subtitle: 'Desarrollador Front-end',
-};
-
-// ABOUT DATA
-export const aboutData = {
-  sectionTitle: "SOBRE MI",
-  paragraphOne: " Hola! Soy Pablo, desarrollador front-end. Aprendí de programación (con Python y R) mientras realizaba mi master en estadística. De ahí, comencé a aprender desarrollo web por afición y me quedé por pasión. A día de hoy me gustaría hacer de esta pasión un trabajo.",
-  paragraphTwo: " Principalmente, uso HTML, CSS y Javascript. Para la maquetación me apoyo en SASS y para estructurar los proyectos me gusta el framework de React con Gatsby. Para estilo uso Materialize y Material UI principalmente. Finalemnte, para control de versión uso Git, aunque durante mi tiempo de trabajo usé Tortoise SVN.",
-  paragraphThree: '',
+  description: ' Desarrollador front-end en España. Abierto a colaboraciones y proyectos. ¡Si necesitas un sitio web no dudes en contactarme! ', 
 };
